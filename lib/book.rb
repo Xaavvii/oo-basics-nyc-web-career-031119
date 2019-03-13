3a15ffbbd8b0db2816dbbf3555024d33a0777c18
authentic-actor-8801 <xaviernational@aol.com>
2019-03-13 21:29:33 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,17 @@
+class Shoe
+  attr_accessor :brand, :color, :material, :condition
+  attr_reader :size
 
+  def initialize(brand)
+    @brand = brand
+  end
 
+  def cobble
+    puts "Your shoe is as good as new!"
+    @condition = "new"
+  end
+
+  def size=(shoe_size)
+    @size = shoe_size
+  end
+end
